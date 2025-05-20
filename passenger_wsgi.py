5c0a9ecf8c7f502e15b46fc0914fc62c71422805
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2025-05-20 10:28:02 -0400

    change version

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Path to your virtual environment's site-packages
-venv_site_packages = "/home/mspinega/ondemand/dev/drone-image_processing-api/venv/lib/python3.12/site-packages"
+venv_site_packages = "/home/mspinega/ondemand/dev/drone-image_processing-api/venv/lib/python3.9/site-packages"
 
 # Add venv site-packages to sys.path
 sys.path.insert(0, venv_site_packages)
