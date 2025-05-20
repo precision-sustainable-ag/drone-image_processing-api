686eec8df7511ebcd87c24a5a8d4fbeb211545fc
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2025-05-20 10:16:06 -0400

    change passenger config

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,12 @@
-# passenger_wsgi.py
 import sys
-sys.path.insert(0, "/home/mspinega/ondemand/dev/drone-image-processing-api")
+import os
+
+# Activate your virtual environment
+venv_path = "/home/mspinega/ondemand/dev/drone-image_processing-api/venv"
+activate_this = os.path.join(venv_path, "bin", "activate_this.py")
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Add your app to the path
+sys.path.insert(0, "/home/mspinega/ondemand/dev/drone-image_processing-api")
 
 from app import app as application  # must be named "application"
